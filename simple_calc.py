ec2ea2aee1a8425f2c253501af4230bcb28e79cf
Shaun Liew Khye Yang <161272658+MrAFirm@users.noreply.github.com>
2025-06-25 11:08:37 +0800

    Update simple_calc.py
    Added a program loop.

--- simple_calc.py
+++ simple_calc.py
@@ -2,22 +2,29 @@ print("Welcome to my calculator!")
 
 print("\n1.Addition\n2.Subtraction\n3.Multiplication\n4.Division\n")
 
-operator = input("Please Select the number# of operator for calculation.\n")
+program_loop = True
 
-num1 = int(input("First Number: "))
-num2 = int(input("Second Number: "))
-
-if operator == "1":
-	result = num1 + num2
-	print(f"Total after addition is: {result}")
-elif operator == "2":
-	result = num1 - num2
-	print(f"Total after subtraction is: {result}")
-elif operator == "3":
-	result = num1 * num2
-	print(f"Total after multiplication is: {result}")
-elif operator == "4":
-	result = num1 / num2
-	print(f"Total after division is: {result}")
-else:
-	print("\nWrong input!\n")
+while program_loop:
+	operator = input("Please Select the number# of operator for calculation.\n")
+	
+	num1 = int(input("First Number: "))
+	num2 = int(input("Second Number: "))
+	
+	if operator == "1":
+		result = num1 + num2
+		print(f"Total after addition is: {result}")
+		program_loop = False
+	elif operator == "2":
+		result = num1 - num2
+		print(f"Total after subtraction is: {result}")
+		program_loop = False
+	elif operator == "3":
+		result = num1 * num2
+		print(f"Total after multiplication is: {result}")
+		program_loop = False
+	elif operator == "4":
+		result = num1 / num2
+		print(f"Total after division is: {result}")
+		program_loop = False
+	else:
+		print("\nWrong input!\n")
